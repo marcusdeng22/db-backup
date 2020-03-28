4f5fd2763082dc79440c54efc399e2bd2e311ca0
marcus <marcusd.9234@gmail.com>
2020-03-28 07:37:09 +0000

    updated pull and commit process

--- do-backup.py
+++ do-backup.py
@@ -26,25 +26,62 @@ if __name__ == "__main__":
 	with open("config.json", "r") as f:
 		config = json.load(f)
 
+	'''
 	#make directories if they don't exist
 	if not os.path.exists(OUTPUT):
 		print("making output folder")
 		os.makedirs(OUTPUT)
+	'''
+
 	print("starting")
 	#check when last run; if missed then try immediately
 	#setup listener socket
 
+	#make dump folder
+	gitPath = os.path.join(OUTPUT, config["database"])
+	if not os.path.exists(gitPath):
+		os.makedirs(gitPath)
+
+	with cd(gitPath):
+		subprocess.run(["git", "init"], check=True)
+		#check if local username/email set
+		try:
+			subprocess.run(["grep", "\[user\]", ".git/config"], check=True)
+		except subprocess.CalledProcessError:
+			print("initializing username")
+			subprocess.run(["git", "config", "--local", "user.name", config["username"]])
+			print("initializing email")
+			subprocess.run(["git", "config", "--local", "user.email", config["email"]])
+
+		try:
+			subprocess.run(["git", "remote", "add", "origin", config["remote"]], check=True)
+		except subprocess.CalledProcessError:
+			print("remote already set")
+
+		subprocess.run(["git", "fetch", "origin"])
+		subprocess.run(["git", "checkout", config["branch"]])
+
 	#move this to a thread eventually that will sleep and wake up and do the following
 	subprocess.run(["mongodump", "--db", config["database"], "--out", OUTPUT], check=True)
 	print("dump OK")
-	with cd(os.path.join(OUTPUT, config["database"])):
-		subprocess.run(["git", "init"], check=True)
+	with cd(gitPath):
+		subprocess.run(["git", "pull"])
+
+		print("git add")
 		subprocess.run(["git", "add", "-A"], check=True)
+		print("git status")
 		status = subprocess.run(["git", "status"], check=True, stdout=subprocess.PIPE)
-		if b"Changes to be commited" in status.stdout:
+
+		if b"Changes to be committed" in status.stdout:
 			print("changes detected, so pushing")
 			subprocess.run(["git", "commit", "-m", str(datetime.datetime.now())], check=True)
-			subprocess.run(["git", "remote", "add", "origin", config["remote"]], check=True)
 			subprocess.run(["git", "push", "-u", "origin", config["branch"]], check=True)
 			print("pushed changes!")
+		elif b"No commits yet" in status.stdout:
+			print("no commits yet, so pushing")
+			subprocess.run(["git", "commit", "-m", str(datetime.datetime.now())], check=True)
+			subprocess.run(["git", "push", "-u", "origin", config["branch"]], check=True)
+		else:
+			print(status.stdout)
 	#update last run
+
